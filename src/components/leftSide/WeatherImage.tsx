4f978293ea76f92281bd0d97b125766f3230d2d0
Suka <mateodefalco1@gmail.com>
2023-09-06 15:05:53 -0300

    WeatherImages creation

--- src/components/leftSide/WeatherImage.tsx
+++ src/components/leftSide/WeatherImage.tsx
@@ -1,5 +1,48 @@
+import { TWeatherImages, TWeatherImage } from "../../types/IWeatherImages"
+
+const weatherImages: TWeatherImages = {
+	images: [
+		{
+			src: "/cloudy.svg",
+			alt: "Cloudy",
+		},
+		{
+			src: "/night.svg",
+			alt: "Night",
+		},
+		{
+			src: "/rainy.svg",
+			alt: "Rainy",
+		},
+		{
+			src: "/stormy.svg",
+			alt: "Stormy",
+		},
+		{
+			src: "/cloudyRainy.svg",
+			alt: "CloudyRainy",
+		},
+		{
+			src: "/sunyCloudy.svg",
+			alt: "SunyCloudy",
+		},
+		{
+			src: "/sunny.svg",
+			alt: "Sunny",
+		},
+	],
+}
+
 const WeatherImage = () => {
-	return <div>WeatherImage</div>
+	return (
+		<div className="flex justify-center items-center">
+			<img
+				src={weatherImages.images[0].src}
+				alt={weatherImages.images[0].alt}
+				style={{ maxWidth: "100px" }}
+			/>
+		</div>
+	)
 }
 
 export default WeatherImage
---
+++ src/types/IWeatherImages.tsx
@@ -0,0 +1,8 @@
+export type TWeatherImage = {
+	src: string
+	alt: string
+}
+
+export type TWeatherImages = {
+	images: TWeatherImage[]
+}
