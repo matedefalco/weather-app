2a97ac312b8dcec9b26200de873fe9941298844a
Suka <mateodefalco1@gmail.com>
2023-09-07 10:25:51 -0300

    DayFilter creation

--- src/components/rightSide/filterBar/DayFilter.tsx
+++ src/components/rightSide/filterBar/DayFilter.tsx
@@ -1,5 +1,32 @@
+import { useState } from "react"
+
 const DayFilter = () => {
-	return <div>DayFilter</div>
+	const [activeTab, setActiveTab] = useState<string>("Today")
+
+	const handleTabClick = (tabName: string): void => {
+		setActiveTab(tabName)
+	}
+
+	return (
+		<div className="tabs p-4">
+			<a
+				className={`tab tab-bordered ${
+					activeTab === "Today" ? "tab-active" : ""
+				}`}
+				onClick={() => handleTabClick("Today")}
+			>
+				Today
+			</a>
+			<a
+				className={`tab tab-bordered ${
+					activeTab === "Week" ? "tab-active" : ""
+				}`}
+				onClick={() => handleTabClick("Week")}
+			>
+				Week
+			</a>
+		</div>
+	)
 }
 
 export default DayFilter
