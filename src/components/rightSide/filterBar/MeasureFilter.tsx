aaf43e6534b49bba2db82584664e6184ee9c61a6
Suka <mateodefalco1@gmail.com>
2023-09-07 10:30:44 -0300

    MeasureFilter creation

--- src/components/rightSide/filterBar/MeasureFilter.tsx
+++ src/components/rightSide/filterBar/MeasureFilter.tsx
@@ -1,5 +1,14 @@
 const MeasureFilter = () => {
-	return <div>MeasureFilter</div>
+	return (
+		<div className="p-4">
+			<label className="btn btn-circle swap swap-rotate">
+				{/* this hidden checkbox controls the state */}
+				<input type="checkbox" />
+				<p className="swap-off fill-current">ºC</p>
+				<p className="swap-on fill-current">ºF</p>
+			</label>
+		</div>
+	)
 }
 
 export default MeasureFilter
