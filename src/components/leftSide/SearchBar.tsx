5a184acd08535505e88eb62eeb6e4795ea6fc715
Suka <mateodefalco1@gmail.com>
2023-09-07 10:17:51 -0300

    UI SearchBar

--- src/components/leftSide/SearchBar.tsx
+++ src/components/leftSide/SearchBar.tsx
@@ -1,16 +1,16 @@
 const SearchBar = () => {
 	return (
-		<div className="join w-full">
-			<div>
-				<div>
+		<div className="w-[90%]">
+			<div className="join bg-gray-200 rounded-lg overflow-hidden w-full">
+				<div className="flex w-full">
 					<input
-						className="input input-bordered join-item"
+						className="grow input input-bordered join-item"
 						placeholder="Search"
 					/>
 				</div>
-			</div>
-			<div className="indicator">
-				<button className="btn join-item">Search</button>
+				<div className="indicator">
+					<button className="btn join-item">Search</button>
+				</div>
 			</div>
 		</div>
 	)
